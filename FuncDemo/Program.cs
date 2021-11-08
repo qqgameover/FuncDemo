4321812b94367548376d001702f998cbb8d36995
qqgameover <kasper35@live.no>
2021-11-08 19:53:58 +0100

    Initial commit

---
+++ FuncDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FuncDemo
+{
+    static class Program
+    {
+        public static Func<int, int, int> Func = Thing;
+        static void Main(string[] args)
+        {
+            var x = Test(30, 40);
+            Console.WriteLine(x);
+            var merText = new List<string> { "dette ", "er ", "en test " };
+            var text = new StringBuilder()
+                .Append("Hei og hallo, ")
+                .AddSequence(merText, (builder, c) => builder
+                    .Append(c));
+            Console.WriteLine(text);
+        }
+
+        public static int Thing(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Test(int n1, int n2)
+        {
+            return Func(n1, n2);
+        }
+
+        public static StringBuilder AddSequence<T>(this StringBuilder @this,
+            IEnumerable<T> seq,
+            Func<StringBuilder, T, StringBuilder> fn) => seq.Aggregate(@this, fn);
+    }
+}
