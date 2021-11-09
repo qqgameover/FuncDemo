3289d6507a76af834294e6b7d41aa335eb6669f8
qqgameover <kasper35@live.no>
2021-11-09 15:30:42 +0100

    Update Program.cs

--- FuncDemo/Program.cs
+++ FuncDemo/Program.cs
@@ -10,11 +10,22 @@ namespace FuncDemo
         static void Main(string[] args)
         {
             var merText = new List<string> {"dette ", "er ", "en test "};
+            var testArray = new[] {1, 3, 4, 7, 9, 10, 15, 19};
             AddTwoNums(30, 40).Tee(Console.WriteLine);
             new StringBuilder()
                 .Append("Hei og hallo, ")
                 .AddSequence(merText, (builder, c) => builder
                     .Append(c)).Tee(Console.WriteLine);
+
+            var intList = Enumerable.Range(0, testArray.Length)
+                .Select(x => testArray[x])
+                .Where(x => x > 10);
+            foreach (var num in intList) Console.WriteLine(num);
+
+            var enumerable = testArray.Select(x => x)
+                .Where(x => x > 10);
+            foreach (var i in enumerable) Console.WriteLine(i);
+            foreach (var num in testArray) Console.WriteLine(num);
         }
         
         public static Func<int, int, int> AddTwoNums = (num1, num2) => num1 + num2; //????? but why....
